6667010fa1b6daac2183355e0916987fac522b94
vboya-CLGX <vboya@corelogic.com>
2024-01-08 08:06:49 +0530

    example-2

---
+++ example-2/terraform.tf
@@ -0,0 +1,17 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  required_version = "~> 1.2.0"
+}
